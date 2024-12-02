b877b3c22d6e25582c6c9dde97f054b7f15fce68
shadowisf <lespaul021504@gmail.com>
2024-12-02 19:05:11 +0400

    fixed post images not loading on startup

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -23,6 +23,7 @@ export default function Home() {
     async function fetchPostInfo() {
       setIsPageLoading(true);
       await onRefresh();
+      setImageKey(imageKey + 1);
       setIsPageLoading(false);
     }
 
